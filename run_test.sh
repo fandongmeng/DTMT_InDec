de416ef2c8c98869d5447c3472032b9499f8134c
Fandong Meng <16472872+fandongmeng@users.noreply.github.com>
2019-06-06 12:07:14 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@ train_dir=$work_dir/data
 export PYTHONPATH=$work_dir/$code_dir:$PYTHONPATH
 export CUDA_VISIBLE_DEVICES=0
 
-for idx in `seq 1000 1000 1000` 
+for idx in `seq 2000 2000 2000` 
 do
     echo model_checkpoint_path: \"model.ckpt-$idx\" > $work_dir/train/checkpoint
     cat $work_dir/train/checkpoint
@@ -14,7 +14,7 @@ do
         --checkpoints $work_dir/train \
         --input $train_dir/valid_src \
         --output $train_dir/valid_src.out \
-        --vocabulary $train_dir/vocab_32k.ch $train_dir/vocab_32k.en \
+        --vocabulary $train_dir/vocab.ch $train_dir/vocab.en \
         --parameters=decode_batch_size=64
     echo evaluating with checkpoint-$idx
     cd $train_dir
